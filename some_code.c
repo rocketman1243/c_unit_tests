8d7d76105ec9ea1f95bb652135905454884637a8
Timon Bestebreur <timonbestebreur@gmail.com>
2019-11-04 18:56:45 +0100

    Fix outdated comment

--- some_code.c
+++ some_code.c
@@ -6,7 +6,9 @@ int timesTwo(int n) {
     return 2 * n;
 }
 
-// copies and pastes the name after the name
+// writes a string to the given char pointer
+// purpose of this method is to also create a unit test
+// 		for comparing the result of a string
 char * greetMe(char * target) {
 	target = "Hi, there!";
 	return target;
